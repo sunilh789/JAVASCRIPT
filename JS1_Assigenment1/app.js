71c4ebeaa59758d84bf917e91b83fb1ca1fe7d26
hsunilkumar <sunil917777@gmail.com>
2017-01-17 21:24:22 +0530

    JAVASCRIPT 1 assignments
    Front End Basics and JAVA script 1 assignments

---
+++ JS1_Assigenment1/app.js
@@ -0,0 +1,9 @@
+var name="sunil patel";
+var age=25;
+var dob="11-11-1991";
+var pob="Bengaluru"
+
+console.log(name);
+console.log(age);
+console.log(dob);
+console.log(pob);
---
+++ JS3_Assigenment3/app.js
@@ -0,0 +1,22 @@
+var emp={
+	
+              name:"sunilpatel"	,
+			  age:25,
+			  salary:2000,
+			  address:{
+				          city:"Bengaluru",
+				          state:"Karnataka",
+						  pincode:560100
+			          }
+	 
+         };
+		 
+		 console.log("Employee Name: "+emp.name);
+		 console.log("Employee Age: "+emp.age);
+		 console.log("Employee Salary: "+emp.salary);
+		 console.log("****Employee Address****");
+		 console.log("City: "+emp.address.city);
+		 console.log("State: "+emp.address.state);
+		 console.log("Pincode: "+emp.address.pincode);
+		 
+		 
